666c6cdc89d4ff3921ed2a419e588574e68bbc66
cynthia <cchelangat9892gmail.com>
2022-08-27 12:07:35 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name,email) {
+    let formData = {
+        name : name,
+        email : email
+     };
+     const configurationObject = {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify(formData),
+
+     };
+      return fetch("http://localhost:3000/users", configurationObject)
+     .then(function(response) {
+        return response.json()
+     })
+     .then(formData => {
+        let id = formData["id"]
+        let body = document.querySelector("body")
+        body.innerHTML = id;
+     })
+     .catch(function (error) {
+        alert("Unauthorized Access");
+        document.body.innerHTML = error.message
+     });
+
+}
+submitData("Chelah", "chelangat")
